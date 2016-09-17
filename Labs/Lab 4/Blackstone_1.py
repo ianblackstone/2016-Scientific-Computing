c6249817afb36352d2a4ef3271d825a65f3b8fc2
[ianblackstone] <ianblackstone@boisestate.edu>
2016-09-16 19:07:44 -0600

    Lab 4 work

---
+++ Labs/Lab 4/Blackstone_1.py
@@ -0,0 +1,28 @@
+# Imports
+import matplotlib.pyplot as plt
+import numpy as np
+
+# define half life and step size in years
+tau = 75
+h = 1
+
+# Create a list with the initial value in the first cell
+N = [1E6]
+
+# Calculate the next value of N using the previous value of N
+for t in np.arange(0,999,h):
+	N.append(N[t] - h*N[t]/tau)
+
+# Generate a figure with two subplots and save the resulting figure
+plt.figure(1)
+plt.subplot(211).set_title('Number of atoms over time (linear)')
+plt.plot(np.arange(0,1000,h),N)
+plt.xlabel('years')
+plt.ylabel('Number of atoms')
+plt.subplot(212).set_title('Number of atoms over time (log)')
+plt.semilogy(np.arange(0,1000,h),N)
+plt.xlabel('years')
+plt.ylabel('Number of atoms')
+plt.tight_layout()
+plt.savefig(filename='plot1.png')
+plt.show()
---
+++ Labs/Lab 4/Blackstone_2.py
@@ -0,0 +1,25 @@
+# Imports
+import matplotlib.pyplot as plt
+import numpy as np
+
+# define half life and step size in years
+tau = 3.82
+h = 0.0001
+flag = 0
+
+def f(N):
+	return -N/tau
+
+# Create a list with the initial value in the first cell
+Nlist = [250]
+tlist = np.arange(0,30,h)
+
+# Calculate the next value of N using the previous value of N
+for t in range(0,len(tlist)):
+	N = Nlist[t]
+	k = h*f(N)
+	k2 = h*f(N + k/2)
+	Nlist.append(N + (k+k2)/2)
+	if N < 100 and flag == 0:
+		print(tlist[t])
+		flag = 1

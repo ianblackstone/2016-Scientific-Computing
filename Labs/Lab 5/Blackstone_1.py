6416c9ff888d3b3f78f7c9d7649086b5a26408d2
[ianblackstone] <ianblackstone@boisestate.edu>
2016-09-23 19:01:06 -0600

    Lab 5
    Files for lab 5, need to be commented fully later.

---
+++ Labs/Lab 5/Blackstone_1.py
@@ -0,0 +1,53 @@
+# Imports
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Declare constants
+g = -9.8
+V = 100
+theta = 45
+Vxi = np.cos(theta)*V
+Vyi = np.sin(theta)*V
+xi = 0
+yi = 0
+
+# Set initial values for x and y
+r = np.array([xi,yi,Vxi,Vyi],float)
+
+# Define a function of r that returns the values of y and dy
+def f(r,t):
+	x = r[0]
+	y = r[1]
+	dx = r[2]
+	dy = r[3]
+	d2x = 0
+	d2y = g
+	return np.array([dx,dy,d2x,d2y],float)
+
+# Set range, interval size, and initial conditions
+a = 0.0
+b = 18.0
+N = 1000
+h = (b-a)/N
+
+# Create an array of time points and declare two empty lists.
+tpoints = np.arange(a,b,h)
+ypts = []
+xpts = []
+
+# For each time point track the values of x and y, then evaluate the function at the next points
+# and set out current value of y and x equal to the output of our function at the midpoint of each bin
+for t in tpoints:
+	ypts.append(r[0])
+	xpts.append(r[1])
+	k1 = h*f(r,t)
+	k2 = h*f(r+0.5*k1,t)
+	r += k2
+
+# Plot the data
+plt.plot(ypts,xpts)
+plt.xlabel("x")
+plt.ylabel('y')
+plt.title('Trajectory')
+plt.ylim(ymin=0)
+plt.show()
---
+++ Labs/Lab 5/Blackstone_2.py
@@ -0,0 +1,56 @@
+# Imports
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Declare constants
+g = -9.8
+V = 100
+theta = [15,30,45,60]
+colors = ['red','blue','green','black']
+xi = 0
+yi = 0
+
+# Set range, interval size, and initial conditions
+a = 0.0
+b = 20.0
+N = 1000
+h = (b-a)/N
+tpoints = np.arange(a,b,h)
+
+for num in range(0,len(theta)):
+	Vxi = np.cos(np.pi/180 * theta[num])*V
+	Vyi = np.sin(np.pi/180 * theta[num])*V
+
+	# Create an array of time points and declare two empty lists.
+	ypts = []
+	xpts = []
+
+	# Set initial values for x and y
+	r = np.array([xi,yi,Vxi,Vyi],float)
+
+	# Define a function of r that returns the values of y and dy
+	def f(r,t):
+		x = r[0]
+		y = r[1]
+		dx = r[2]
+		dy = r[3]
+		d2x = 0
+		d2y = g
+		return np.array([dx,dy,d2x,d2y],float)
+
+	# For each time point track the values of x and y, then evaluate the function at the next points
+	# and set out current value of y and x equal to the output of our function at the midpoint of each bin
+	for t in tpoints:
+		ypts.append(r[0])
+		xpts.append(r[1])
+		k1 = h*f(r,t)
+		k2 = h*f(r+0.5*k1,t)
+		r += k2
+	# Plot the data
+	plt.plot(ypts,xpts,color=colors[num])
+
+plt.xlabel("x")
+plt.ylabel('y')
+plt.title('Trajectory')
+plt.ylim(ymin=0)
+plt.show()

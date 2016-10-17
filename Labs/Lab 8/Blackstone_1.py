59673c679c703658fb8dfea40ab955b82374e648
Ian Blackstone <ianblackstone@boisestate.edu>
2016-10-17 02:30:33 -0600

    Lab 8

--- Labs/Lab 8/Blackstone_1.py
+++ Labs/Lab 8/Blackstone_1.py
@@ -1,45 +1,42 @@
-#Code adapted from Newman text
+# Imports
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 
-#import empty,zeros,max
-#from pylab import imshow,gray,show
-
 # Constants
-M = 10          # Grid squares on a side
-V = 10          # Voltage at top wall
-target = 1e-6   # Target accuracy
+M = 100
+V = 10
+target = 1e-6
 
 # Create arrays to hold potential values
 phi = np.zeros([M+1,M+1],float)
 phi[-1,:] = V
 phi[:,-1] = V
-phiprime = np.empty([M+1,M+1],float)
 
 # Main loop
 delta = 1.0
+w = 0.5
 ncount = 0
 
 while delta>target:
+	phiold=np.copy(phi)
 
-    # Calculate new values of the potential
-    for i in range(M+1):
-        for j in range(M+1):
-            if i==0 or i==M or j==0 or j==M:
-                phiprime[i,j] = phi[i,j]
-            else:
-                phiprime[i,j] = (phi[i+1,j] + phi[i-1,j] + phi[i,j+1] + phi[i,j-1])/4
+	# Calculate new values of the potential
+	for i in range(M+1):
+		for j in range(M+1):
+			if i == 0 or i == M or j == 0 or j == M:
+				phi[i,j] = phi[i,j]
+			else:
+				phi[i,j] = (1. + w) * (phi[i + 1, j] + phi[i - 1 ,j] + phi[i,j + 1] + phi[i,j - 1])/4 - w*phi[i,j]
 
-    # Calculate maximum difference from old values
-    delta = np.max(abs(phi-phiprime))
+	# Calculate maximum difference from old values
+	delta = np.max(abs(phi-phiold))
+	ncount+=1
 
-    # Swap the two arrays around
-    phi,phiprime = phiprime,phi
-    ncount+=1
-
-# Make a plot
-print(ncount)
-plt.imshow(phi)
-plt.gray()
+# Make a plot of the contour lines
+cont = plt.contour( phi , 15 , colors='black')
+plt.title('Equipotential contours using Laplace\'s method')
+plt.gca().invert_yaxis()
+plt.clabel(cont, inline=True, fontsize=10)
 plt.show()
+plt.savefig(filename='plot1.png')
--- Labs/Lab 8/Blackstone_2.py
+++ Labs/Lab 8/Blackstone_2.py
@@ -1,2 +1,49 @@
-import scipy.interpolate.interpld as inter
+# imports
+import scipy.interpolate as inter
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
 
+# Constants
+M = 100
+V = 10
+target = 1e-6
+
+# Create arrays to hold potential values
+phi = np.zeros([M+1,M+1],float)
+phi[-1,:] = V
+phi[:,-1] = V
+
+# Main loop
+delta = 1.0
+w = 0.5
+ncount = 0
+
+while delta>target:
+	phiold=np.copy(phi)
+
+	# Calculate new values of the potential
+	for i in range(M+1):
+		for j in range(M+1):
+			if i == 0 or i == M or j == 0 or j == M:
+				phi[i,j] = phi[i,j]
+			else:
+				phi[i,j] = (1. + w) * (phi[i + 1, j] + phi[i - 1 ,j] + phi[i,j + 1] + phi[i,j - 1])/4 - w*phi[i,j]
+
+	# Calculate maximum difference from old values
+	delta = np.max(abs(phi-phiold))
+	ncount+=1
+
+# Create an interpolation for the potential at the X midpoint of the cell.
+X = np.arange(0,M+1)
+Y = phi[:,int(M/2)]
+Yinterp = inter.interp1d(X,Y)
+Xnew = np.arange(0,M,0.1)
+
+# Plot the interpolation
+plt.plot(Xnew,Yinterp(Xnew))
+plt.title('Potential at X = 0.5 as a function of Y')
+plt.ylabel('potential')
+plt.xlabel('Y')
+plt.show()
+plt.savefig(filename='plot2.png')

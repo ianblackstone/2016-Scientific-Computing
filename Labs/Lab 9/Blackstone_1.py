c515488a10d78ec6aa7dae8ec56904884eede79b
[ianblackstone] <ianblackstone@boisestate.edu>
2016-10-21 17:58:42 -0600

    Lab 9

---
+++ Labs/Lab 9/Blackstone_1.py
@@ -0,0 +1,31 @@
+import numpy.random as random
+import matplotlib.pyplot as plt
+
+m = 1000
+b = 10
+x = range(1,b)
+
+n = random.rand(m)
+
+nav = []
+tar = []
+npr = []
+
+for k in x:
+	nk = n**k
+	nav.append(sum(nk)/m)
+	tar.append(1/(k+1))
+	ni = []
+	for i in range(0,m-1):
+		if i+k > m-1:
+			s = i-k
+		else:
+			s = i+k
+		ni.append(n[i]*n[s])
+	npr.append(sum(ni)/m)
+
+plt.plot(x,nav,'b-',x,tar,'ro')
+plt.show()
+
+plt.plot(x,npr,'b-')
+plt.show()

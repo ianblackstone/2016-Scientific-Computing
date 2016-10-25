621cc3d123e830d9b2e8f65dff92e0c22b0e5024
[ianblackstone] <ianblackstone@boisestate.edu>
2016-10-25 10:50:35 -0600

    Lab 9

--- Labs/Lab 9/Blackstone_2.py
+++ Labs/Lab 9/Blackstone_2.py
@@ -8,8 +8,8 @@ steps = 2*np.pi*np.random.rand(nwalk,nsteps,2)
 xsteps = np.zeros((nwalk,nsteps))
 ysteps = np.zeros((nwalk,nsteps))
 
-for i in range(nwalk):          #loop over walkers
-    for j in range(1,nsteps):        #loop over steps all start at 0,0
+for i in range(nwalk):          # loop over walkers
+    for j in range(1,nsteps):        # loop over steps all start at 0,0
       xsteps[i,j]=xsteps[i,j-1] + np.cos(steps[i,j,0])
       ysteps[i,j]=ysteps[i,j-1] + np.sin(steps[i,j,1])
 
@@ -22,3 +22,6 @@ plt.savefig('plot3.png')
 plt.show()
 
 dist = np.sqrt(np.square(xsteps[:,-1]) + np.square(ysteps[:,-1]))
+a = np.histogram(dist)
+plt.hist(a)
+plt.show()

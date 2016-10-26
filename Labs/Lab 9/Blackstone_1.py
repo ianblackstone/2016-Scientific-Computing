c9350839565f69a42d4d49de051946c93ef4b804
[ianblackstone] <ianblackstone@boisestate.edu>
2016-10-26 14:18:05 -0600

    Lab 9 done

--- Labs/Lab 9/Blackstone_1.py
+++ Labs/Lab 9/Blackstone_1.py
@@ -1,33 +1,58 @@
+# imports
 import numpy.random as random
 import matplotlib.pyplot as plt
 
+# Set number of random numbers and number of loops.
 m = 1000
-b = 10
+b = 11
 x = range(1,b)
 
+# Generate a list of random numbers.
 n = random.rand(m)
 
+# Create three empty lists, average, target, and product.
 nav = []
 tar = []
 npr = []
 
+# calculate n^k for each value of k as well as the expected value of the average.
 for k in x:
 	nk = n**k
 	nav.append(sum(nk)/m)
 	tar.append(1/(k+1))
+	# create an empty list for this value of k.
 	ni = []
+	# fill the empty list ni with the products of each cell of n and its kth neighbor.
 	for i in range(0,m-1):
+		# Avoid calling for values of n that don't exist by checking that our indexes are not out of range.
 		if i+k > m-1:
-			s = i-k
+			s = i-2*k
 		else:
 			s = i+k
+		# append the product of n and its kth neighbor (or -2*kth neighbor for the last k values of n)
 		ni.append(n[i]*n[s])
 	npr.append(sum(ni)/m)
 
-plt.plot(x,nav,'b-',x,tar,'ro')
+# Plot the average value
+plt.plot(x,nav,'b-',label='Average')
+plt.plot(x,tar,'ro',label='Expected')
+plt.xlabel('K')
+plt.ylabel('average value of n^k')
+plt.title('average n^k by k')
+plt.legend()
 plt.savefig('plot1.png')
 plt.show()
 
-plt.plot(x,npr,'b-')
+# plot the average product
+plt.plot(x,npr,'b-',label='Average product')
+plt.plot([1,x[-1]],[0.25,0.25],'--',label='Expected')
+plt.xlabel('K')
+plt.ylabel('average product')
+plt.title('average product of n and its kth neighbor')
+plt.ylim(ymax = 0.5,ymin = 0)
+plt.legend()
 plt.savefig('plot2.png')
 plt.show()
+
+# Both of these test show that the random numbers being generated are
+# independent and well distributed within the range.
--- Labs/Lab 9/Blackstone_3.py
+++ Labs/Lab 9/Blackstone_3.py
@@ -8,24 +8,15 @@ nsteps = 100
 
 # generate random directions for each step and generate a list of zeros for the x and y positions
 steps = 2*np.pi*np.random.rand(nwalk,nsteps,2)
+augstep = np.random.normal(1,0.2,[nwalk,nsteps])
 xsteps = np.zeros((nwalk,nsteps))
 ysteps = np.zeros((nwalk,nsteps))
 
 # for each walker calculate the position after each step.
 for i in range(nwalk):
     for j in range(1,nsteps):
-      xsteps[i,j]=xsteps[i,j-1] + np.cos(steps[i,j,0])
-      ysteps[i,j]=ysteps[i,j-1] + np.sin(steps[i,j,1])
-
-# Plot the first 10 walkers
-for k in range(10):
-    plt.plot(xsteps[k,:],ysteps[k,:])    
-plt.xlabel('x positions');
-plt.ylabel('y position');
-plt.title('Paths of the first 10 walkers')
-plt.tight_layout()
-plt.savefig('plot3.png')
-plt.show()
+      xsteps[i,j]=xsteps[i,j-1] + augstep[i,j]*np.cos(steps[i,j,0])
+      ysteps[i,j]=ysteps[i,j-1] + augstep[i,j]*np.sin(steps[i,j,1])
 
 # Calculate the distance moved by each walker
 dist = np.sqrt(np.square(xsteps[:,-1]) + np.square(ysteps[:,-1]))
@@ -40,7 +31,8 @@ plt.ylabel('Number of walkers')
 plt.title('Number of walkers to travel a given distance')
 plt.legend()
 plt.tight_layout()
-plt.savefig('plot4.png')
+plt.savefig('plot5.png')
 plt.show()
 
-# The histogram does not necissarily peak at the expected value, but the actual average is close to the expected value.
+# The histogram has the same average as we expect, same as the excersize 2, but now the
+# distribution is more reliably grouped around the expected average.

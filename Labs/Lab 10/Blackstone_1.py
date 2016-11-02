bb215b5c800911de485bd80f23ed96ec81ac2527
[ianblackstone] <ianblackstone@boisestate.edu>
2016-11-02 15:32:08 -0600

    Lab 10 done

--- Labs/Lab 10/Blackstone_1.py
+++ Labs/Lab 10/Blackstone_1.py
@@ -6,18 +6,19 @@ import matplotlib.pyplot as plt
 nwalk = 100
 nsteps = 100
 
-
 # initialize a list.
 xy = np.zeros((nwalk,nsteps,2))
 
-# for each walker.
+# For each walker.
 for i in range(nwalk):
 
-	# for each step
+	# For each step
 	for j in range(1,nsteps):
-		# clear the possible steps.
+
+		# Clear the possible steps.
 		steps = []
-		# check if each step can be taken, if yes add it to the list of possible moves.
+
+		# Check if each step can be taken, if yes add it to the list of possible moves.
 		if [ xy[i,j-1,0] + 1 , xy[i,j-1,1] ] not in np.ndarray.tolist(xy[i,:]):
 			steps.append([1,0])
 
@@ -29,15 +30,16 @@ for i in range(nwalk):
 
 		if [ xy[i,j-1,0] , xy[i,j-1,1] - 1 ] not in np.ndarray.tolist(xy[i,:]):
 			steps.append([0,-1])
+
 		# If no steps available abandon this walker.
 		if steps == []:
 			xy[i,j:nsteps] = xy[i,j-1]
 			break
+
 		# Randomly choose a step and add the result of that step to our position list.
 		step = steps[np.random.randint(len(steps))]
 		xy[i,j] = [ xy[i,j-1,0] + step[0] , xy[i,j-1,1] + step[1] ]
 
-print(xy)
 # Plot the first 10 walkers
 for k in range(10):
 	plt.plot(xy[k,:,0],xy[k,:,1])
@@ -45,7 +47,7 @@ plt.xlabel('x positions');
 plt.ylabel('y position');
 plt.title('Paths of the first 10 walkers')
 plt.tight_layout()
-plt.savefig('plot3.png')
+plt.savefig('plot_1.png')
 plt.show()
 
 # Calculate the distance moved by each walker
@@ -53,7 +55,6 @@ dist = np.sqrt(np.square(xy[:,-1,0]) + np.square(xy[:,-1,1]))
 
 # Create a histogram of the distance traveled for each walker.
 plt.hist(dist)
-plt.axvline(np.sqrt(nsteps),color='r',label='Expected average')
 plt.axvline(np.sqrt(np.mean(np.square(dist))),color='g',label='Average')
 plt.xlim(xmin=0,xmax=max(dist))
 plt.xlabel('Distance traveled')
@@ -61,7 +62,5 @@ plt.ylabel('Number of walkers')
 plt.title('Number of walkers to travel a given distance')
 plt.legend()
 plt.tight_layout()
-plt.savefig('plot4.png')
+plt.savefig('plot_2.png')
 plt.show()
-
-# The histogram does not necissarily peak at the expected value, but the actual average is close to the expected value.
--- Labs/Lab 10/Blackstone_2.py
+++ Labs/Lab 10/Blackstone_2.py
@@ -1,23 +1,33 @@
+# Imports
 import numpy.random as random
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Generate a list of N values and a list of zeros.
 N = range(1,1000)
 estimates = np.zeros(N[-1])
 
+# Create a function that estimates pi.
 def estimatepi(N):
+	# generate an xy position with -1 to 1 and find the distance to the point.
 	xpos = 2*random.rand(N) - 1
 	ypos = 2*random.rand(N) - 1
 	dist = np.sqrt(np.square(xpos)+np.square(ypos))
+
+	# Count the points in and out of a circle of radius 1.
 	incircle = sum([i for i in dist <= 1])
 	outcircle = sum([i for i in dist > 1])
+
+	# Prevent division by 0.
 	if outcircle == 0:
 		outcircle = 1
 	return incircle/outcircle
 
+# Estimate pi for N points.
 for n in N:
 	estimates[n-1] = estimatepi(n)
 
+# Plot the estimates and the known value of pi.
 plt.plot(N,estimates,'b-',label='estimate')
 plt.plot(N,np.pi*np.ones(N[-1]),'r-',label='pi',linewidth=2)
 plt.ylabel('pi estimate')
@@ -25,5 +35,5 @@ plt.xlabel('N')
 plt.title('estimate of pi using the circle method for N points')
 plt.legend()
 plt.tight_layout()
-plt.savefig('plot_2.png')
+plt.savefig('plot_3.png')
 plt.show()

a3ded01adb00d06b03cbe08c2fadb453eadd3e52
[ianblackstone] <ianblackstone@boisestate.edu>
2016-11-09 11:41:42 -0700

    Lab 11 done

--- Labs/Lab 11/Blackstone_1.py
+++ Labs/Lab 11/Blackstone_1.py
@@ -2,36 +2,44 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-ncells = 2560  #number of sites to fill
-nside = 256   #width of the grid
-# r = 15
+ncells = 256  # number of sites to fill
+nside = 256   # width of the grid
 
-body=np.zeros((nside,nside))-1  #array holding the cell occupancy counter (-1=empty; 0 = candidate; 1=filled)
-age=np.zeros((nside,nside))    #make an array that holds the step number for which the cell is occupied
+body = np.zeros((nside,nside)) - 1  # array holding the cell occupancy counter (-1=empty; 0 = candidate; 1=filled)
+age = np.zeros((nside,nside))    # make an array that holds the step number for which the cell is occupied
 
-nsteps = nside-10
-neighbors=np.array([
+# Maximum number of steps a walker should take.
+nsteps = 100
+
+# An array containing the relative coordinates of neighboring cells.
+neighbors = np.array([
 	(0, 1), 
 	(0, -1), 
 	(1, 0), 
 	(-1, 0)
 ])
 
-ctr=int(nside/2)
+# Find the center and place a nucleation site there. 
+ctr = int(nside/2)
 body[ctr,ctr] = 1
+
+# set the nieghboring cells as possible growth sites.
 for near in neighbors:
 	body[ctr+near[0],ctr+near[1]] = 0
 
+# A loop that repeats until we reach ncells filled cells.
 for i in range(ncells-1):
+	# reset the flag for a found point.
 	flag = 0
+	# Try spawning random walkers until a cell is filled.
 	while flag == 0:
-		# theta = 2*np.pi*np.random.rand()
+		# Spawn a walker in a random location.
 		position = [int(round(nside*np.random.rand())),int(round(nside*np.random.rand()))]
-		# position = [int(round(r*np.cos(theta))),int(round(r*np.sin(theta)))]
 		for j in range(1,nsteps):
-			# Randomly choose a step and add the result of that step to our position list.
+			# Randomly choose a step and add the result of that step to the previous position.
 			step = neighbors[np.random.randint(4)]
 			propstep = [ position[0] + step[0] , position[1] + step[1] ]
+			# check if the walker has run out of steps, is out of bounds, or is in a candidate cell.
 			if j == nsteps:
 				break
 			elif propstep[0] <= 0 or propstep[0] >= nside or propstep[1] <= 0 or propstep[1] >= nside:
@@ -39,21 +47,21 @@ for i in range(ncells-1):
 			elif body[ propstep[0], propstep[1] ] == 0:
 				flag = 1
 				break
+			# If none of the above take the proposed step.
 			else:
 				position = propstep 
-	# if i < 237:
-	# 	r += 1
-	# 	nsteps += 1
+	# Set The candidate cell to filled.
 	body[ position[0], position[1] ] = 1
-	for near in neighbors:    #now check the neighbors, and if -1 change to 0
+
+	for near in neighbors:    # now check the neighbors, and if -1 change to 0
 		if body[ position[0] + near[0], position[1] + near[1] ] == -1:
 			body[ position[0] + near[0], position[1] + near[1] ] = 0
-	age[position[0],position[1]]= ncells-i   #record the age for the filled site at the end of the run
-	
+	age[position[0],position[1]] = ncells-i   # record the age for the filled site at the end of the run
 
+unfilled = np.where(body == -1)  # find non-filled/non-candidate sites
+body[unfilled] = 0             # set them equal to zero
 
-unfilled=np.where(body == -1)  #find non-filled/non-candidate sites
-body[unfilled] = 0             #set them equal to zero
-
-plt.imshow(age,cmap=plt.cm.spectral)
+# Plot the data.
+plt.imshow(age,cmap = plt.cm.spectral)
+plt.savefig("plot_1.png")
 plt.show()

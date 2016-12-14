b2b0b8ef8649d361cc31fd6ed8e46d13187e9aad
[ianblackstone] <ianblackstone@boisestate.edu>
2016-12-13 17:31:16 -0700

    We have orbit!
    sort of

--- Labs/Project_2.py
+++ Labs/Project_2.py
@@ -25,45 +25,45 @@ c = 1
 r0 = 156.8
 dr0 = 0
 th0 = 0
-dt0 = 1.23E-6
+dt0 = -0.7
 
 # Create a list of times
-times = np.linspace(0,5000,20000000)
+times = np.linspace(0,500,20000000)
 
 # Store the initial conditions in an array.
-r0 = [r0,th0,dr0,dt0]
+p0 = [r0,th0,dr0,dt0]
 
-h = r0[3]*r0[0]**2
+h = p0[3]*p0[0]**2
 
 # Define a function to find the derivative of each component of r.
-def Edr(r,t):
+def Edr(p,t):
 
 	# initialize an array of zeros.
-	v = np.zeros_like(r)
+	v = np.zeros_like(p)
 
-	v[0] = r[2]
-	v[1] = r[3]
+	v[0] = p[2]
+	v[1] = p[3]
 
-	v[2] = -1/( m/(h)**2 + 3*m/r[0]**3 - 1/r[0])
+	v[2] = -1/( m/(h)**2 + 3*m/p[0]**3 - 1/p[0])
 	v[3] = 0
 	
 	return v
 
-def Ndr(r,t):
+def Ndr(p,t):
 
-	v = np.zeros_like(r)
+	v = np.zeros_like(p)
 
-	v[0] = r[2]
-	v[1] = r[3]
+	v[0] = p[2]
+	v[1] = p[3]
 
 	# Newtonian equations of motion
-	v[2] = -M/r[0]**2 + h
-	v[3] = 0
+	v[2] = -M/p[0]**2
+	v[3] = -2*p[3]*p[3]/p[0]
 	return v
 
 # Call the ODE solver
-Newt = inter.odeint(Ndr,r0,times)
-Ein = inter.odeint(Edr,r0,times)
+Newt = inter.odeint(Ndr,p0,times)
+Ein = inter.odeint(Edr,p0,times)
 
 plt.plot(Newt[:,0]*np.cos(Newt[:,1]),Newt[:,0]*np.sin(Newt[:,1]))
 plt.plot(0,0,'ro')
